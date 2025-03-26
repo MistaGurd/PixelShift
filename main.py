ab610d96e0375911985f1b6147171f5807f740fe
MistaGurd <144097182+MistaGurd@users.noreply.github.com>
2025-03-26 14:33:43 +0100

    Opsætning af Main

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from kivy.app import app
+# Diverse imports
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
+from kivy.core.window import Window
 
+# Importering af klasser fra de andre Python filer
+from BGfjernelse import RemBG
+# from Filkompromering import...
+# from Formatkonvertering import
+# from PDF_Merge import
+class MainMenu(Screen):
+    pass
 class PixelShiftApp(App):
     def build(self):
-        Builder.load_file("GUI.kv")
-        return PixelShift()
+        Window.maximize()  # Får programmet til at åbne i Windowded borderless
+        sm = ScreenManager()  # Variabel for håndtering af start-skærm, og resultatskærm
+        sm.add_widget(MainMenu(name="Hovedmenu"))
+        sm.add_widget(RemBG(name="Baggrundsfjernelse"))
+        return sm
 
 
 if __name__ == "__main__":
