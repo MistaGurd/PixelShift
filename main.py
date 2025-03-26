24153b9be6bef803bdf9286d434572c838f27430
MistaGurd <144097182+MistaGurd@users.noreply.github.com>
2025-03-26 15:08:55 +0100

    Opsætning af Main

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ from BGfjernelse import RemBG
 # from Filkompromering import...
 # from Formatkonvertering import
 # from PDF_Merge import
+
+
+
 class MainMenu(Screen):
     pass
 class PixelShiftApp(App):
     def build(self):
         Window.maximize()  # Får programmet til at åbne i Windowded borderless
         sm = ScreenManager()  # Variabel for håndtering af start-skærm, og resultatskærm
-        sm.add_widget(MainMenu(name="Hovedmenu"))
-        sm.add_widget(RemBG(name="Baggrundsfjernelse"))
+        sm.add_widget(MainMenu())
+        sm.add_widget(RemBG())
+        sm.transition = SwapTransition()
         return sm
 
 
